4bc6260e6d8de0a851dffcaa23003476dc80aa54
tsucchon <tsuchiya@magbridge.com>
2026-01-03 01:25:08 +0900

    todoの連携機能追加

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,3 +1,37 @@
+import { gql } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client/react";
+import { useState } from "react";
+
+const GET_TODOS = gql`
+  query {
+    getTodos {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const ADD_TODO = gql`
+  mutation addTodo($title: String!) {
+    addTodo(title: $title) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const UPDATE_TODO = gql`
+  mutation updateTodo($id: ID!, $completed: Boolean!) {
+    updateTodo(id: $id, completed: $completed) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
 type Todo = {
   id: string;
   title: string;
@@ -5,26 +39,56 @@ type Todo = {
 };
 
 function App() {
-  const todos = [
-    { id: "1", title: "GraphQLを学ぶ", completed: false },
-    { id: "2", title: "Reactを学ぶ", completed: true },
-  ] as Todo[];
+  const { loading, data } = useQuery<{ getTodos: Todo[] }>(GET_TODOS, {
+    fetchPolicy: "network-only",
+  });
+
+  const todos = data ? data.getTodos : [];
+  const [addTodo] = useMutation(ADD_TODO);
+  const [updateTodo] = useMutation(UPDATE_TODO);
+  const [title, setTitle] = useState("");
+
+  if (loading) return <p>Loading...</p>;
+
+  const handleAddTodo = async () => {
+    await addTodo({
+      variables: { title },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+    setTitle("");
+  };
+
+  const handleUpdateTodo = async (id: string, completed: boolean) => {
+    await updateTodo({
+      variables: { id, completed: !completed },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+  };
 
   return (
     <>
       <div>
         <h1>TO DO List</h1>
-        <input type="text" placeholder="TODOを追加してください" />
-        <button>追加</button>
+        <input
+          type="text"
+          placeholder="TODOを追加してください"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>追加</button>
         <ul>
-          {todos.map((todo) => (
+          {todos.map((todo: Todo) => (
             <li
               key={todo.id}
               style={{
                 textDecoration: todo.completed ? "line-through" : "none",
               }}
             >
-              <input type="checkbox" checked={todo.completed} />
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleUpdateTodo(todo.id, todo.completed)}
+              />
               {todo.title}
             </li>
           ))}
--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "/graphql" }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
